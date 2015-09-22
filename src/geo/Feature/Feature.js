15738329fc928bb28d3db17f52bc98d8702288af
citolen <nils.citoleux@epitech.eu>
2015-09-22 16:17:54 +0200

    Improved events management

--- src/geo/Feature/Feature.js
+++ src/geo/Feature/Feature.js
@@ -21,9 +21,13 @@ C.Geo.Feature.Feature = C.Utils.Inherit(function (base, type, options) {
 
     this._metadata = options.metadata || {};
 
+    this._interactive = options.interactive || false;
+
 }, EventEmitter, 'C.Geo.Feature.Feature');
 
 C.Geo.Feature.Feature.OpacityMask = 1024;
+C.Geo.Feature.Feature.InteractiveMask = 2048;
+C.Geo.Feature.Feature.InteractiveEvents = ['click', 'mousedown', 'mousemove', 'mouseup'];
 
 C.Geo.Feature.Feature.EventType = {
     ADDED: 0,
@@ -36,6 +40,24 @@ C.Geo.Feature.Feature.FeatureType = {
     IMAGE: 1,
     LINE: 2,
     POLYGON: 3
+};
+
+C.Geo.Feature.Feature.prototype.on = function (eventName) {
+    if (!this._interactive && C.Geo.Feature.Feature.InteractiveEvents.indexOf(eventName) != -1) {
+        this._mask |= C.Geo.Feature.Feature.InteractiveMask;
+        this._interactive = true;
+        this.makeDirty();
+    }
+    EventEmitter.prototype.on.apply(this, arguments);
+};
+
+C.Geo.Feature.Feature.prototype.once = function (eventName) {
+    if (!this._interactive && C.Geo.Feature.Feature.InteractiveEvents.indexOf(eventName) != -1) {
+        this._mask |= C.Geo.Feature.Feature.InteractiveMask;
+        this._interactive = true;
+        this.makeDirty();
+    }
+    EventEmitter.prototype.once.apply(this, arguments);
 };
 
 C.Geo.Feature.Feature.prototype.addTo = function (container) {
@@ -70,6 +92,18 @@ C.Geo.Feature.Feature.prototype.opacity = function (opacity) {
 C.Geo.Feature.Feature.prototype.makeDirty = function () {
     this._dirty = true;
     this.emit('dirty', this);
+};
+
+C.Geo.Feature.Feature.prototype.__mousedown = function (event) {
+    this.emit('mousedown', this, event);
+};
+
+C.Geo.Feature.Feature.prototype.__mousemove = function (event) {
+    this.emit('mousemove', this, event);
+};
+
+C.Geo.Feature.Feature.prototype.__mouseup = function (event) {
+    this.emit('mouseup', this, event);
 };
 
 C.Geo.Feature.Feature.prototype.__click = function (event) {
